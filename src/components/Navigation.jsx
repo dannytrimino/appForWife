d8405e7edbb8c199ee7f29432f34d9e85d4f0c6e
Danny Trimino <47509205+dannytrimino@users.noreply.github.com>
2022-03-18 10:33:51 -0400

    tiddyUpCode change script tag to be more efficient.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 
-function Navigation(){
+function Navigation() {
   return (
     <ul className="nav nav-pills">
       <li className="nav-item">
-        <a href="www.google.com" className="nav-link active" ariaCurrent="page">Home</a>
-        </li>
+        <a
+          href="www.google.com"
+          className="nav-link active"
+          aria-current="page"
+        >
+          Home
+        </a>
+      </li>
       <li className="nav-item">
-        <a href="www.google.com" className="nav-link">Features</a>
-        </li>
+        <a href="www.google.com" className="nav-link">
+          Features
+        </a>
+      </li>
       <li className="nav-item">
-        <a href="www.google.com" className="nav-link">Pricing</a>
-        </li>
+        <a href="www.google.com" className="nav-link">
+          Pricing
+        </a>
+      </li>
       <li className="nav-item">
-        <a href="www.google.com" className="nav-link">FAQs</a>
-        </li>
+        <a href="www.google.com" className="nav-link">
+          FAQs
+        </a>
+      </li>
       <li className="nav-item">
-        <a href="www.google.com" className="nav-link">About</a>
-        </li>
+        <a href="www.google.com" className="nav-link">
+          About
+        </a>
+      </li>
     </ul>
-  )
+  );
 }
 export default Navigation;
