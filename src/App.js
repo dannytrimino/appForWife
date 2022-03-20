260595dc02ec7a6d044d95d44fbcd82e3ff2e23b
Danny Trimino <47509205+dannytrimino@users.noreply.github.com>
2022-03-20 09:50:57 -0400

    Add Table.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Table from "./components/Table";
 
 function App() {
   return (
     <div>
       <Header />
+      <Table />
     </div>
   );
 }
